bcc253b009b765e1fdc2a4387afc85530f0ae50c
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2023-08-15 00:07:36 +0100

    Remove feedbacks from authUserState

--- src/interfaces/state/authUserState.ts
+++ src/interfaces/state/authUserState.ts
@@ -1,10 +1,8 @@
 import userState from './userState'
-import feedbackState from './feedbackState'
 import workSetupState from './workSetupState'
 import chatState from './chatState'
 
 export default interface authUserState extends userState {
     workSetup: workSetupState
-    feedbacks: feedbackState[]
     chats: chatState[]
 }
