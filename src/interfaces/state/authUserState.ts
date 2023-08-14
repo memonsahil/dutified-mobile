cbc6ff0761cdfc01e1845ec7b6cdda9d79e2f204
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2023-08-15 00:03:30 +0100

    Add chats to authUserState

--- src/interfaces/state/authUserState.ts
+++ src/interfaces/state/authUserState.ts
@@ -1,8 +1,10 @@
 import userState from './userState'
 import feedbackState from './feedbackState'
 import workSetupState from './workSetupState'
+import chatState from './chatState'
 
 export default interface authUserState extends userState {
     workSetup: workSetupState
     feedbacks: feedbackState[]
+    chats: chatState[]
 }
