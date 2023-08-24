8b195794efbedc8f33091f840ed929c4dfdf763d
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2023-08-24 01:16:13 +0100

    Remove old colours & update ratings enum

--- src/enums/ratings.ts
+++ src/enums/ratings.ts
@@ -1,9 +1,9 @@
 enum ratings {
-    five = '5_STARS',
-    four = '4_STARS',
-    three = '3_STARS',
-    two = '2_STARS',
-    one = '1_STAR',
+    FIVE = '5_STARS',
+    FOUR = '4_STARS',
+    THREE = '3_STARS',
+    TWO = '2_STARS',
+    ONE = '1_STAR',
 }
 
 export default ratings
--- src/theme/colors.ts
+++
@@ -1,7 +0,0 @@
-const raisinBlack = '#1A1A23'
-const yellowGreen = '#86CD4D'
-const platinum = '#E3E3E3'
-const jet = '#333333'
-const silver = '#A9A9A9'
-
-export { raisinBlack, yellowGreen, platinum, jet, silver }
