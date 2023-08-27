0d011255a5f7ebfa96b656918ad1ca526d15fb41
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2023-08-27 15:31:48 +0100

    Update screen types

---
+++ src/screens/types/userScreenProps.ts
@@ -0,0 +1,8 @@
+import { RouteProp } from '@react-navigation/native'
+import screens from '../../params/screens'
+
+type userScreenProps = {
+    route: RouteProp<screens, 'User'>
+}
+
+export default userScreenProps
