e725b6efee3ae397106ec9ab13e768773143a245
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2023-08-27 03:20:37 +0100

    Replace theme with themeColors & fontSizes

---
+++ src/enums/fontSizes.ts
@@ -0,0 +1,12 @@
+enum fontSizes {
+    HEADING_ONE = 34,
+    HEADING_TWO = 26,
+    HEADING_THREE = 22,
+    BODY_ONE = 18,
+    BODY_TWO = 16,
+    BODY_THREE = 14,
+    BUTTON = 24,
+    INPUT = 20,
+}
+
+export default fontSizes
