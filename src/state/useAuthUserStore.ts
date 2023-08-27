369c86b068cb71e26dd3be3aa12018b5760ea3ed
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2023-08-27 03:04:26 +0100

    Remove old store and create new one

---
+++ src/state/useAuthUserStore.ts
@@ -0,0 +1,61 @@
+import { create } from 'zustand'
+import authUserType from '../data/types/authUserType'
+
+const initialState: authUserType = {
+    account: {
+        userId: '',
+        firstName: '',
+        lastName: '',
+        countryCode: '',
+        phoneNumber: '',
+        emailAddress: '',
+    },
+    profile: {
+        userName: '',
+        profilePicture: '',
+        description: '',
+        interests: [],
+        affiliation: {},
+    },
+    jobsCreated: [],
+    jobsWorked: [],
+    feedbacks: [],
+    chats: [],
+    payments: [],
+}
+
+const useAuthUserStore = create()((set) => ({
+    ...initialState,
+    updateAuthUser: async (authUser: authUserType) => {
+        set((state: authUserType) => ({
+            ...state,
+            account: {
+                ...state.account,
+                userId: authUser.account.userId,
+                firstName: authUser.account.firstName,
+                lastName: authUser.account.lastName,
+                countryCode: authUser.account.countryCode,
+                phoneNumber: authUser.account.phoneNumber,
+                emailAddress: authUser.account.emailAddress,
+            },
+            profile: {
+                ...state.profile,
+                userName: authUser.profile.userName,
+                profilePicture: authUser.profile.profilePicture,
+                description: authUser.profile.description,
+                interests: authUser.profile.interests,
+                affiliation: authUser.profile.affiliation,
+            },
+            jobsCreated: authUser.jobsCreated,
+            jobsWorked: authUser.jobsWorked,
+            feedbacks: authUser.feedbacks,
+            chats: authUser.chats,
+            payments: authUser.payments,
+        }))
+    },
+    removeAuthUser: async () => {
+        set(initialState)
+    },
+}))
+
+export default useAuthUserStore
