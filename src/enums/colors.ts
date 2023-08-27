83e00c3e42bd12879374398d2749170747ae499e
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2023-08-27 03:10:18 +0100

    Replace colors & fonts with theme enum

--- src/enums/colors.ts
+++
@@ -1,9 +0,0 @@
-enum colors {
-    RAISIN_BLACK = '#1A1A23',
-    YELLOW_GREEN = '#86CD4D',
-    PLATINUM = '#E3E3E3',
-    JET = '#333333',
-    SILVER = '#A9A9A9',
-}
-
-export default colors
---
+++ src/enums/theme.ts
@@ -0,0 +1,20 @@
+enum theme {
+    // Colors
+    RAISIN_BLACK = '#1A1A23',
+    YELLOW_GREEN = '#86CD4D',
+    PLATINUM = '#E3E3E3',
+    JET = '#333333',
+    SILVER = '#A9A9A9',
+
+    // Font sizes
+    HEADING_ONE = 34,
+    HEADING_TWO = 26,
+    HEADING_THREE = 22,
+    BODY_ONE = 18,
+    BODY_TWO = 16,
+    BODY_THREE = 14,
+    BUTTON = 24,
+    INPUT = 20,
+}
+
+export default theme
