6717ecd4449197c1979da8546dba2fd86404460d
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2023-09-01 00:56:14 +0100

    Format themeColors

--- src/enums/themeColors.ts
+++ src/enums/themeColors.ts
@@ -1,9 +1,9 @@
 enum themeColors {
     BLACK = '#000000',
     WHITE = '#FFFFFF',
+    SILVER = '#A9A9A9',
     YELLOW_GREEN = '#89CE50',
     PLATINUM = '#E3E3E3',
-    SILVER = '#A9A9A9',
 }
 
 export default themeColors
