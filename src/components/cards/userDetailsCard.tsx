2d566042fd7dc26c855d090181bd25d3009311db
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2023-09-02 21:57:25 +0100

    Create userDetailCard to show interests & bio

---
+++ src/components/cards/userDetailsCard.tsx
@@ -0,0 +1,55 @@
+import React from 'react'
+import { View, Text, StyleSheet } from 'react-native'
+import fontSizes from '../../enums/fontSizes'
+import themeColors from '../../enums/themeColors'
+import userDetailsCardProps from '../props/userDetailsCardProps'
+
+const userDetailsCard = (props: userDetailsCardProps) => {
+    return (
+        <View style={styles.container}>
+            <Text style={styles.heading}>Bio</Text>
+            <Text style={styles.description}>{props.description}</Text>
+            <Text style={styles.heading}>Interests</Text>
+            {props.interests.map((interest) => (
+                <View style={styles.interestWrapper} key={interest}>
+                    <Text style={styles.interest}>{interest}</Text>
+                </View>
+            ))}
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: themeColors.WHITE,
+        width: '90%',
+        marginTop: '5%',
+        borderRadius: 20,
+        paddingHorizontal: '5%',
+        paddingTop: '5%',
+    },
+    heading: {
+        fontFamily: 'IBMPlexSansCondensed-SemiBold',
+        fontSize: fontSizes.BODY_ONE,
+        color: themeColors.BLACK,
+        marginBottom: '5%',
+    },
+    interestWrapper: {
+        flexDirection: 'row',
+        marginBottom: '5%',
+    },
+    interest: {
+        fontFamily: 'IBMPlexSansCondensed-SemiBold',
+        fontSize: fontSizes.BODY_TWO,
+        color: themeColors.BLACK,
+        backgroundColor: themeColors.YELLOW_GREEN,
+        padding: '1%',
+    },
+    description: {
+        fontSize: fontSizes.BODY_TWO,
+        marginBottom: '5%',
+    },
+})
+
+export default userDetailsCard
---
+++ src/components/props/userDetailsCardProps.ts
@@ -0,0 +1,8 @@
+import categories from '../../enums/categories'
+
+type userDetailsCardProps = {
+    interests: categories[]
+    description: string
+}
+
+export default userDetailsCardProps
