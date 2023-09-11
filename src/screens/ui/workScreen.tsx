f18e96b712502caa1e4a3a9d66aa131f8395edba
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2023-09-12 00:27:22 +0100

    Add projectCards on work screen

--- src/screens/ui/workScreen.tsx
+++ src/screens/ui/workScreen.tsx
@@ -15,12 +15,47 @@ import jobCardProps from '../../components/props/jobCardProps'
 import jobStatus from '../../enums/jobStatus'
 import categories from '../../enums/categories'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
+import projectCardProps from '../../components/props/projectCardProps'
+import ProjectCard from '../../components/cards/projectCard'
 
 const WorkScreen = () => {
     const [switchColumn, setSwitchColumn] = useState<'Hired' | 'Created'>(
         'Hired'
     )
     const navigation: NavigationProp<screens> = useNavigation()
+    const projectsHired: Array<projectCardProps> = [
+        {
+            projectId: '1',
+            projectName: 'Hired Project 1',
+            description: 'This is a description for Hired project 1',
+            creationDate: '2021-01-01',
+            category: categories.ACCOUNTING,
+        },
+        {
+            projectId: '2',
+            projectName: 'Hired Project 2',
+            description:
+                'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ali quam, sit ame sit ame.',
+            creationDate: '2021-01-01',
+            category: categories.ADVERTISING,
+        },
+        {
+            projectId: '3',
+            projectName: 'Hired Project 3',
+            description:
+                'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ali quam, sit ame sit ame.',
+            creationDate: '2021-01-01',
+            category: categories.ADVERTISING,
+        },
+        {
+            projectId: '4',
+            projectName: 'Hired Project 4',
+            description:
+                'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ali quam, sit ame sit ame.',
+            creationDate: '2021-01-01',
+            category: categories.ADVERTISING,
+        },
+    ]
     const jobsHired: Array<jobCardProps> = [
         {
             jobId: '1',
@@ -28,7 +63,7 @@ const WorkScreen = () => {
             status: jobStatus.AVAILABLE,
             payment: '100000',
             description:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ali quam, sit ame sit ame.',
+                'Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ali quam, sit ame sit ame.',
             creationDate: '2021-01-01',
             category: categories.ACCOUNTING,
         },
@@ -51,23 +86,22 @@ const WorkScreen = () => {
             category: categories.ANIMATION,
         },
     ]
+    const projectsCreated: Array<projectCardProps> = [
+        {
+            projectId: '1',
+            projectName: 'Hired Project 1',
+            description: 'This is a description for Hired project 1',
+            creationDate: '2021-01-01',
+            category: categories.ACCOUNTING,
+        },
+    ]
     const jobsCreated: Array<jobCardProps> = [
         {
             jobId: '1',
             jobName: 'Created Job 1',
-            status: jobStatus.AVAILABLE,
-            payment: '100000',
-            description:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ali quam, sit ame sit ame.',
-            creationDate: '2021-01-01',
-            category: categories.ACCOUNTING,
-        },
-        {
-            jobId: '2',
-            jobName: 'Created Job 2',
             status: jobStatus.IN_PROGRESS,
             payment: '200',
-            description: 'This is a description for Created job 2',
+            description: 'This is a description for Created job 1',
             creationDate: '2021-01-01',
             category: categories.ADVERTISING,
         },
@@ -118,55 +152,143 @@ const WorkScreen = () => {
                 </View>
                 {switchColumn === 'Hired' ? (
                     <>
-                        {jobsHired.length === 0 ? (
+                        {projectsHired.length === 0 &&
+                        jobsHired.length === 0 ? (
                             <View style={styles.noDataContainer}>
                                 <Text style={styles.noDataText}>
-                                    Jobs that you are hired for will be shown
-                                    here.
+                                    Projects and jobs that you are hired for
+                                    will be shown here.
                                 </Text>
                             </View>
                         ) : (
                             <>
-                                {jobsHired.map((job) => (
-                                    <JobCard
-                                        key={job.jobId}
-                                        nav={navigation}
-                                        jobId={job.jobId}
-                                        jobName={job.jobName}
-                                        status={job.status}
-                                        payment={job.payment}
-                                        description={job.description}
-                                        creationDate={job.creationDate}
-                                        category={job.category}
-                                    />
-                                ))}
+                                {projectsHired.length !== 0 ? (
+                                    <>
+                                        <Text style={styles.subHeading}>
+                                            Projects
+                                        </Text>
+                                        <ScrollView
+                                            contentContainerStyle={
+                                                styles.projectsContainer
+                                            }
+                                            horizontal={true}
+                                            showsHorizontalScrollIndicator={
+                                                false
+                                            }
+                                        >
+                                            {projectsHired.map((project) => (
+                                                <ProjectCard
+                                                    key={project.projectId}
+                                                    nav={navigation}
+                                                    projectId={
+                                                        project.projectId
+                                                    }
+                                                    projectName={
+                                                        project.projectName
+                                                    }
+                                                    description={
+                                                        project.description
+                                                    }
+                                                    creationDate={
+                                                        project.creationDate
+                                                    }
+                                                    category={project.category}
+                                                />
+                                            ))}
+                                        </ScrollView>
+                                    </>
+                                ) : null}
+                                {jobsHired.length !== 0 ? (
+                                    <>
+                                        <Text style={styles.subHeading}>
+                                            Jobs
+                                        </Text>
+                                        {jobsHired.map((job) => (
+                                            <JobCard
+                                                key={job.jobId}
+                                                nav={navigation}
+                                                jobId={job.jobId}
+                                                jobName={job.jobName}
+                                                status={job.status}
+                                                payment={job.payment}
+                                                description={job.description}
+                                                creationDate={job.creationDate}
+                                                category={job.category}
+                                            />
+                                        ))}
+                                    </>
+                                ) : null}
                             </>
                         )}
                     </>
                 ) : (
                     <>
-                        {jobsCreated.length === 0 ? (
+                        {projectsCreated.length === 0 &&
+                        jobsCreated.length === 0 ? (
                             <View style={styles.noDataContainer}>
                                 <Text style={styles.noDataText}>
-                                    Jobs that are created by you will be shown
-                                    here.
+                                    Projects and jobs that you create will be
+                                    shown here.
                                 </Text>
                             </View>
                         ) : (
                             <>
-                                {jobsCreated.map((job) => (
-                                    <JobCard
-                                        key={job.jobId}
-                                        nav={navigation}
-                                        jobId={job.jobId}
-                                        jobName={job.jobName}
-                                        status={job.status}
-                                        payment={job.payment}
-                                        description={job.description}
-                                        creationDate={job.creationDate}
-                                        category={job.category}
-                                    />
-                                ))}
+                                {projectsCreated.length !== 0 ? (
+                                    <>
+                                        <Text style={styles.subHeading}>
+                                            Projects
+                                        </Text>
+                                        <ScrollView
+                                            contentContainerStyle={
+                                                styles.projectsContainer
+                                            }
+                                            horizontal={true}
+                                            showsHorizontalScrollIndicator={
+                                                false
+                                            }
+                                        >
+                                            {projectsCreated.map((project) => (
+                                                <ProjectCard
+                                                    key={project.projectId}
+                                                    nav={navigation}
+                                                    projectId={
+                                                        project.projectId
+                                                    }
+                                                    projectName={
+                                                        project.projectName
+                                                    }
+                                                    description={
+                                                        project.description
+                                                    }
+                                                    creationDate={
+                                                        project.creationDate
+                                                    }
+                                                    category={project.category}
+                                                />
+                                            ))}
+                                        </ScrollView>
+                                    </>
+                                ) : null}
+                                {jobsCreated.length !== 0 ? (
+                                    <>
+                                        <Text style={styles.subHeading}>
+                                            Jobs
+                                        </Text>
+                                        {jobsCreated.map((job) => (
+                                            <JobCard
+                                                key={job.jobId}
+                                                nav={navigation}
+                                                jobId={job.jobId}
+                                                jobName={job.jobName}
+                                                status={job.status}
+                                                payment={job.payment}
+                                                description={job.description}
+                                                creationDate={job.creationDate}
+                                                category={job.category}
+                                            />
+                                        ))}
+                                    </>
+                                ) : null}
                             </>
                         )}
                     </>
@@ -204,11 +326,26 @@ const styles = StyleSheet.create({
         width: '50%',
         justifyContent: 'space-between',
         paddingTop: '5%',
-        marginBottom: '5%',
     },
     button: {
         fontFamily: 'IBMPlexSansCondensed-Bold',
         fontSize: fontSizes.BUTTON,
+    },
+    subHeading: {
+        fontFamily: 'IBMPlexSansCondensed-Bold',
+        fontSize: fontSizes.HEADING_TWO,
+        color: themeColors.WHITE,
+        marginTop: '5%',
+        marginBottom: '5%',
+        alignSelf: 'flex-start',
+        paddingLeft: '10%',
+    },
+    projectsContainer: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingLeft: 20,
+        width: 'auto',
     },
     noDataContainer: {
         alignItems: 'center',
