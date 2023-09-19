5ecbb99e34eb65f8070ec2c2fb776f9930f2585f
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2023-09-19 17:58:06 +0100

    Update createScreen text

--- src/screens/ui/createScreen.tsx
+++ src/screens/ui/createScreen.tsx
@@ -1,121 +1,89 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import {
     StyleSheet,
     Text,
     View,
-    TextInput,
-    Keyboard,
-    TouchableWithoutFeedback,
     TouchableOpacity,
-    Alert,
     ScrollView,
 } from 'react-native'
-import { KeyboardAwareScrollView } from 'react-native-keyboard-aware-scroll-view'
 import { useNavigation, NavigationProp } from '@react-navigation/native'
-import * as Crypto from 'expo-crypto'
-import * as Progress from 'react-native-progress'
-import DatePicker from 'react-native-date-picker'
 import themeColors from '../../enums/themeColors'
 import fontSizes from '../../enums/fontSizes'
-import categories from '../../enums/categories'
 import screens from '../params/screens'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
 
 const CreateScreen = () => {
-    const [name, setName] = useState<string>('')
-    const [enteredCategory, setEnteredCategory] = useState<string>('')
-    const [selectedCategory, setSelectedCategory] = useState<string>('')
-    const [searchResults, setSearchResults] = useState<string[]>([])
-    const [jobDeadline, setJobDeadline] = useState<string>('')
-    const [showDatePicker, setShowDatePicker] = useState<boolean>(false)
-    const [paymentAmount, setPaymentAmount] = useState<string>('')
-    const [desc, setDesc] = useState<string>('')
-    const [loading, setLoading] = useState<boolean>(false)
-
-    const randomUUID = Crypto.randomUUID()
+    const [switchColumn, setSwitchColumn] = useState<'Project' | 'Job'>(
+        'Project'
+    )
 
     const navigation: NavigationProp<screens> = useNavigation()
 
-    useEffect(() => {
-        if (enteredCategory !== '') {
-            setSearchResults(searchCategories(categories, enteredCategory))
-        } else {
-            setSearchResults(Object.values(categories))
-        }
-    }, [enteredCategory])
-
-    const searchCategories = (
-        categories: Record<string, string>,
-        searchArg: string
-    ) => {
-        let results: string[] = []
-
-        for (const category in categories) {
-            if (
-                categories[category]
-                    .toLowerCase()
-                    .includes(searchArg.toLowerCase())
-            ) {
-                results.push(categories[category])
-            }
-        }
-
-        return results
-    }
-
     return (
         <View style={styles.container}>
-            {loading === false ? (
-                <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
-                    <KeyboardAwareScrollView
-                        enableOnAndroid={true}
-                        contentContainerStyle={styles.scrollView}
-                    >
-                        <View style={styles.headerSection}>
-                            <TouchableOpacity
-                                onPress={() => navigation.goBack()}
-                            >
-                                <MaterialCommunityIcons
-                                    name="chevron-left-circle"
-                                    size={30}
-                                    color={themeColors.YELLOW_GREEN}
-                                />
-                            </TouchableOpacity>
-                            <Text style={styles.heading}>Create P/J</Text>
-                        </View>
-                        <Text style={styles.field}>Description</Text>
-                        <TextInput
-                            placeholder="Describe your job's goal, types of skills required etc."
-                            value={desc}
-                            onChangeText={setDesc}
-                            style={styles.descTextInput}
-                            placeholderTextColor={themeColors.SILVER}
-                            inputMode="text"
-                            multiline
+            <ScrollView contentContainerStyle={styles.scrollView}>
+                <View style={styles.headerSection}>
+                    <TouchableOpacity onPress={() => navigation.goBack()}>
+                        <MaterialCommunityIcons
+                            name="chevron-left-circle"
+                            size={30}
+                            color={themeColors.YELLOW_GREEN}
                         />
-                        <TouchableOpacity
-                            style={styles.launchButtonContainer}
-                            onPress={() => {}}
-                        >
-                            <Text style={styles.button}>Create</Text>
-                        </TouchableOpacity>
-                        <Text style={styles.textSection}>
-                            Once your job is created, it can be viewed by other
-                            users and these details cannot be changed later.
-                        </Text>
-                    </KeyboardAwareScrollView>
-                </TouchableWithoutFeedback>
-            ) : (
-                <View style={styles.loadingContainer}>
-                    <Progress.Bar
-                        width={250}
-                        height={25}
-                        borderRadius={20}
-                        indeterminate={true}
-                        color={themeColors.YELLOW_GREEN}
-                    />
+                    </TouchableOpacity>
+                    <Text style={styles.heading}>Create</Text>
                 </View>
-            )}
+                <View style={styles.buttonSection}>
+                    <TouchableOpacity
+                        onPress={() => setSwitchColumn('Project')}
+                    >
+                        <Text
+                            style={{
+                                ...styles.button,
+                                color:
+                                    switchColumn === 'Project'
+                                        ? themeColors.YELLOW_GREEN
+                                        : themeColors.SILVER,
+                            }}
+                        >
+                            Project
+                        </Text>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => setSwitchColumn('Job')}>
+                        <Text
+                            style={{
+                                ...styles.button,
+                                color:
+                                    switchColumn === 'Job'
+                                        ? themeColors.YELLOW_GREEN
+                                        : themeColors.SILVER,
+                            }}
+                        >
+                            Job
+                        </Text>
+                    </TouchableOpacity>
+                </View>
+                <View style={styles.infoSection}>
+                    {switchColumn === 'Project' ? (
+                        <>
+                            <Text style={styles.infoText}>
+                                Create a new project and add multiple jobs:
+                            </Text>
+                            <Text style={styles.infoText}>
+                                Break down your project goal into multiple jobs
+                                and build your team to work on it.
+                            </Text>
+                            <Text style={styles.infoText}>
+                                Collaborate with others, track your project's
+                                progress and manage your hiring budget.
+                            </Text>
+                        </>
+                    ) : (
+                        <Text style={styles.infoText}>
+                            Create a new job and hire a user:
+                        </Text>
+                    )}
+                </View>
+            </ScrollView>
         </View>
     )
 }
@@ -137,52 +105,35 @@ const styles = StyleSheet.create({
     headerSection: {
         flexDirection: 'row',
         paddingTop: '20%',
-        paddingLeft: 30,
-        paddingRight: 30,
-        height: 150,
-        width: 400,
+        paddingLeft: '10%',
+        paddingRight: '10%',
+        width: '100%',
         alignItems: 'center',
     },
     heading: {
         fontFamily: 'IBMPlexSansCondensed-Bold',
-        fontSize: fontSizes.HEADING_TWO,
+        fontSize: fontSizes.HEADING_ONE,
         color: themeColors.WHITE,
-        paddingLeft: 20,
-        paddingRight: 30,
+        paddingLeft: '5%',
     },
-    field: {
-        alignSelf: 'flex-start',
-        paddingLeft: 40,
-        paddingTop: '7%',
-        fontFamily: 'IBMPlexSansCondensed-Bold',
-        fontSize: fontSizes.BUTTON,
-        color: themeColors.WHITE,
-    },
-    descTextInput: {
-        fontFamily: 'IBMPlexSansCondensed-Medium',
-        fontSize: fontSizes.BODY_ONE,
-        color: themeColors.WHITE,
-        width: '100%',
-        height: '100%',
-        overflow: 'visible',
-        padding: 5,
-        textAlignVertical: 'top',
-    },
-    launchButtonContainer: {
-        paddingTop: '10%',
+    buttonSection: {
+        flexDirection: 'row',
+        width: '50%',
+        justifyContent: 'space-between',
+        paddingTop: '5%',
+        paddingBottom: '5%',
     },
     button: {
         fontFamily: 'IBMPlexSansCondensed-Bold',
         fontSize: fontSizes.BUTTON,
-        color: themeColors.YELLOW_GREEN,
     },
-    textSection: {
-        width: '70%',
-        textAlign: 'center',
-        paddingTop: '5%',
+    infoSection: {
         paddingBottom: '20%',
-        fontFamily: 'IBMPlexSansCondensed-Bold',
-        fontSize: fontSizes.BODY_THREE,
+        width: '80%',
+    },
+    infoText: {
+        fontFamily: 'IBMPlexSansCondensed-Medium',
+        fontSize: fontSizes.BODY_ONE,
         color: themeColors.WHITE,
     },
 })
