b7b7fbb6855a5fe608df1afd381585ca8ce51db2
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2023-11-17 00:38:02 +0000

    Update theme colours

--- src/enums/themeColors.ts
+++ src/enums/themeColors.ts
@@ -1,8 +1,8 @@
 enum themeColors {
     BLACK = '#000000',
-    WHITE = '#FFFFFF',
+    WHITE = '#E7E7E7',
     SILVER = '#A9A9A9',
-    YELLOW_GREEN = '#89CE50',
+    YELLOW_GREEN = '#6AAF31',
 }
 
 export default themeColors
