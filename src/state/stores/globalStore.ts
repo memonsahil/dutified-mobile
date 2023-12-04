92c01eada7bb8524c9d96891e6c78c6ade5bb021
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2023-12-04 00:10:34 +0000

    Refactor globalStore to include selectedJob

--- src/state/stores/globalStore.ts
+++ src/state/stores/globalStore.ts
@@ -1,10 +1,16 @@
 import { create } from 'zustand'
 import globalStoreType from '../types/globalStoreType'
 
-const globalStore = create<globalStoreType>((set) => ({
+const initialState = {
     selectedAttachments: [],
+    selectedJob: null,
+}
+
+const globalStore = create<globalStoreType>((set) => ({
+    ...initialState,
     setSelectedAttachments: (attachments) =>
         set(() => ({ selectedAttachments: attachments })),
+    setSelectedJob: (job) => set(() => ({ selectedJob: job })),
 }))
 
 export default globalStore
--- src/state/types/globalStoreType.ts
+++ src/state/types/globalStoreType.ts
@@ -1,8 +1,11 @@
+import jobCardProps from '../../components/props/jobCardProps'
 import attachmentType from '../../data/types/attachmentType'
 
 type globalStoreType = {
     selectedAttachments: attachmentType[]
+    selectedJob: jobCardProps | null
     setSelectedAttachments: (attachments: attachmentType[]) => void
+    setSelectedJob: (job: jobCardProps | null) => void
 }
 
 export default globalStoreType
