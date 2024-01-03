4743a6231b5746952f11c049cd1d4c17417dd4de
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2024-01-03 16:02:04 -0500

    Update profileType

--- src/data/types/profileType.ts
+++ src/data/types/profileType.ts
@@ -1,8 +1,14 @@
+import categories from '../../enums/categories'
+
 type profileType = {
     userId: string
     firstName: string
     lastName: string
     profilePicture: string
+    bio: string
+    ratePerDay: string
+    interests: categories[]
+    links: string[]
     countryCode: string
     phoneNumber: string
     emailAddress: string
