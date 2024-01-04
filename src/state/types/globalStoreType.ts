5a507e8ba9b384b63d2bcb37724fec2a13cf5b37
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2024-01-04 07:13:23 +0100

    Add zustand package and globalStore module

---
+++ src/state/types/globalStoreType.ts
@@ -0,0 +1,15 @@
+import jobCardProps from '../../components/props/jobCardProps'
+import attachmentType from '../../data/types/attachmentType'
+
+type globalStoreType = {
+    selectedAttachments: attachmentType[]
+    showAgreementModal: boolean
+    showJobModal: boolean
+    selectedJob: jobCardProps | null
+    setSelectedAttachments: (attachments: attachmentType[]) => void
+    setShowAgreementModal: (show: boolean) => void
+    setShowJobModal: (show: boolean) => void
+    setSelectedJob: (job: jobCardProps | null) => void
+}
+
+export default globalStoreType
