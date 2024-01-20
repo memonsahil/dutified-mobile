2fa171ac44aa05d95b64805588444a97c1b55f72
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2024-01-20 09:43:06 +0000

    Refactor Firebase SDK configuration

--- app.config.ts
+++ app.config.ts
@@ -1,22 +1,3 @@
-import fs from 'fs'
-
-const decodeAndWriteToFile = (path: string, envVar: string) => {
-    const envValue = process.env[envVar]
-    if (envValue) {
-        const content = Buffer.from(envValue, 'base64').toString('utf-8')
-        fs.writeFileSync(path, content)
-    }
-}
-
-decodeAndWriteToFile(
-    './firebase/GoogleService-Info.plist',
-    'IOS_FIREBASE_SDK_CONFIG'
-)
-decodeAndWriteToFile(
-    './firebase/google-services.json',
-    'ANDROID_FIREBASE_SDK_CONFIG'
-)
-
 export default {
     expo: {
         name: 'Dutified',
@@ -32,11 +13,11 @@ export default {
         assetBundlePatterns: ['**/*'],
         ios: {
             bundleIdentifier: 'com.dutified',
-            googleServicesFile: 'IOS_FIREBASE_SDK_CONFIG',
+            googleServicesFile: process.env.IOS_FIREBASE_SDK_CONFIG,
         },
         android: {
             package: 'com.dutified',
-            googleServicesFile: 'ANDROID_FIREBASE_SDK_CONFIG',
+            googleServicesFile: process.env.ANDROID_FIREBASE_SDK_CONFIG,
             adaptiveIcon: {
                 foregroundImage: './assets/images/adaptive-icon.png',
                 backgroundColor: '#1A1A23',
@@ -64,7 +45,7 @@ export default {
         ],
         extra: {
             eas: {
-                projectId: '5315d69e-9cf7-416d-912c-8638ce458fee',
+                projectId: process.env.EAS_PROJECT_ID,
             },
         },
     },
