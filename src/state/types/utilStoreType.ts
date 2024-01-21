762507544b380013e46bb10b14fcd844bcee6392
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2024-01-21 22:10:51 +0000

    Remove signedIn property from utilStore

--- src/state/types/utilStoreType.ts
+++ src/state/types/utilStoreType.ts
@@ -2,12 +2,10 @@ import jobCardProps from '../../components/props/jobCardProps'
 import attachmentType from '../../data/types/attachmentType'
 
 type utilStoreType = {
-    signedIn: boolean
     selectedAttachments: attachmentType[]
     showAgreementModal: boolean
     showJobModal: boolean
     selectedJob: jobCardProps | null
-    setSignedIn: (signedIn: boolean) => void
     setSelectedAttachments: (attachments: attachmentType[]) => void
     setShowAgreementModal: (show: boolean) => void
     setShowJobModal: (show: boolean) => void
