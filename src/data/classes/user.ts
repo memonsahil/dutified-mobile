172004b8e1323100693f8a2bb36a5d4777000e23
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2024-02-10 00:29:09 +0000

    Define getUserResults

--- src/data/classes/user.ts
+++ src/data/classes/user.ts
@@ -1,8 +1,44 @@
 import requestStatus from '../../enums/requestStatus'
 import UserInterface from '../interfaces/userInterface'
 import promiseType from '../types/promiseType'
+import firestore from '@react-native-firebase/firestore'
+import userType from '../types/userType'
 
 class User implements UserInterface {
+    getUserResults = async (details: {
+        searchQuery: string
+    }): Promise<promiseType> => {
+        try {
+            const matchingUsers: userType[] = []
+            const querySnapshot = await firestore().collection('users').get()
+
+            querySnapshot.forEach((doc) => {
+                if (
+                    (
+                        doc.data().profile?.firstName +
+                        ' ' +
+                        doc.data().profile?.lastName
+                    )
+                        .toLowerCase()
+                        .includes(details.searchQuery.toLowerCase())
+                ) {
+                    matchingUsers.push(doc.data() as userType)
+                }
+            })
+
+            if (matchingUsers.length > 0) {
+                return {
+                    status: requestStatus.SUCCESS,
+                    data: matchingUsers,
+                }
+            } else {
+                return { status: requestStatus.ERROR }
+            }
+        } catch (error: Object | any) {
+            return { status: requestStatus.ERROR, errorCode: error.code }
+        }
+    }
+
     getUser = async (details: { userId: string }): Promise<promiseType> => {
         // Implement get user data logic here
         return { status: requestStatus.SUCCESS }
