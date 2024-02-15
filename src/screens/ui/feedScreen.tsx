bab0904d52facc17f161992aa3dddba1a5d05fcf
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2024-02-15 16:03:51 +0000

    Fetch feedPosts from currentUser

--- src/screens/ui/feedScreen.tsx
+++ src/screens/ui/feedScreen.tsx
@@ -9,70 +9,14 @@ import { useNavigation, NavigationProp } from '@react-navigation/native'
 import themeColors from '../../enums/themeColors'
 import fontSizes from '../../enums/fontSizes'
 import screens from '../params/screens'
-import postCardProps from '../../components/props/postCardProps'
 import PostCard from '../../components/cards/postCard'
 import { MaterialCommunityIcons } from '@expo/vector-icons'
-import attachment from '../../enums/attachment'
+import authStore from '../../state/stores/authStore'
 
 const FeedScreen = () => {
     const navigation: NavigationProp<screens> = useNavigation()
-    const posts: Array<postCardProps> = [
-        {
-            postId: '1',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ali quam, sit ame sit ame.',
-            userId: '1',
-            userName: 'Sahil Memon',
-            userAvatar: '',
-            date: '2021-01-01',
-            attachments: [
-                {
-                    id: '1',
-                    title: 'Created Job 1',
-                    type: attachment.JOB,
-                },
-                {
-                    id: '2',
-                    title: 'Created Project 1',
-                    type: attachment.PROJECT,
-                },
-            ],
-            comments: '1',
-        },
-        {
-            postId: '2',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ali quam, sit ame sit ame.',
-            userId: '1',
-            userName: 'Sahil Memon',
-            userAvatar: '',
-            date: '2021-01-01',
-            attachments: [
-                {
-                    id: '1',
-                    title: 'Created Job 1',
-                    type: attachment.JOB,
-                },
-                {
-                    id: '2',
-                    title: 'Created Job 2',
-                    type: attachment.JOB,
-                },
-            ],
-            comments: '1',
-        },
-        {
-            postId: '3',
-            content:
-                'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec euismod, nisl eget ali quam, sit ame sit ame.',
-            userId: '1',
-            userName: 'Sahil Memon',
-            userAvatar: '',
-            date: '2021-01-01',
-            attachments: [],
-            comments: '1',
-        },
-    ]
+
+    const currentUser = authStore((state) => state.currentUser)
 
     return (
         <View style={styles.container}>
@@ -109,16 +53,16 @@ const FeedScreen = () => {
                         </TouchableOpacity>
                     </View>
                 </View>
-                {posts.length === 0 ? (
+                {currentUser?.feedPosts?.length === 0 ? (
                     <View style={styles.noDataContainer}>
                         <Text style={styles.noDataText}>
-                            Posts that are shared by your network and created by
-                            you will be shown here.
+                            Posts that you create and are shared by your network
+                            will be shown here.
                         </Text>
                     </View>
                 ) : (
                     <>
-                        {posts.map((post) => (
+                        {currentUser?.feedPosts?.map((post) => (
                             <PostCard
                                 key={post.postId}
                                 postId={post.postId}
