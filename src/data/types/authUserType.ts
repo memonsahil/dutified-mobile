9d0d626da9637f0611827d9e4f72174feb2e40b6
Sahil Memon <140768015+memonsahil@users.noreply.github.com>
2024-03-02 22:19:16 +0000

    Create authUserType & update userType

--- src/data/types/authUserType.ts
+++ src/data/types/authUserType.ts
@@ -1,23 +1,38 @@
+import chatType from './chatType'
 import feedbackType from './feedbackType'
 import jobType from './jobType'
+import notificationType from './notificationType'
 import postType from './postType'
 import projectType from './projectType'
+import transactionType from './transactionType'
+import userType from './userType'
 
-type userType = {
+type authUserType = {
     userId?: string
     userName: string
     userAvatar: string
+    countryCode: string
+    phoneNumber: string
+    emailAddress: string
+    cardNumber: string
+    securityCode: string
+    expiryMonth: string
+    expiryYear: string
     userDesc: string
     categories: string[]
     links: string[]
+    notifications: notificationType[]
     network: userType[]
     projectsCreated: projectType[]
     projectsWorked: projectType[]
     jobsCreated: jobType[]
     jobsWorked: jobType[]
     postsCreated: postType[]
+    userFeedPosts: postType[]
+    chats: chatType[]
+    transactions: transactionType[]
     feedbacks: feedbackType[]
     creationDate: string
 }
 
-export default userType
+export default authUserType
